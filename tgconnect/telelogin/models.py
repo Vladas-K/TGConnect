09692d5633b6633d539bfc4a54aaf3978dff193c
Vladas Macbook <v25057777@gmail.com>
2024-12-27 19:12:47 +0300

    Add TelegramUser model

--- tgconnect/telelogin/models.py
+++ tgconnect/telelogin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TelegramUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telegram_id = models.CharField(max_length=100, unique=True)
+    telegram_username = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.telegram_username or self.user.username
+
