2b5ae11f3cde8e91a3df9f2817ee90f68c471d1c
Vladas Macbook <v25057777@gmail.com>
2024-12-30 13:00:47 +0300

    Regitster TelegramUser

--- tgconnect/telelogin/admin.py
+++ tgconnect/telelogin/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TelegramUser
 
-# Register your models here.
+@admin.register(TelegramUser)
+class TelegramUser(admin.ModelAdmin):
+        list_display = ('user', 'telegram_id', 'telegram_username')
+
